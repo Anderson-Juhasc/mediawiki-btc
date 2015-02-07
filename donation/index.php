9485e2ebb55d617c6cb91eb75eb2c9f1255cb68a
Anderson Juhasc <anderson.juhasc@gmail.com>
2015-02-07 17:09:10 -0200

    add donation script

---
+++ donation/index.php
@@ -0,0 +1,108 @@
+<?php
+if ($_POST) {
+    // selecionar um endereço da row
+    $ADDR = "1NrmXPsi9Lez8Z6xfUkWZ412smRANNN14W";
+    // grava a data da exibicao e a data de expiracao com 10min extra no banco
+    $TIME = date("D M d Y H:i:s O");
+    $ENDTIME = date("D M d Y H:i:s O", strtotime($TIME) + 600); // add 10min
+}
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+    <style type="text/css">
+        label {
+            display:block;
+        }
+    </style>
+    <script type="text/javascript" src="../bower_components/jquery/dist/jquery.min.js"></script>
+</head>
+<body>
+<?php if ($_POST) : ?>
+    <h1>Fazer Doação</h1>
+    <p><?php echo $ADDR; ?></p>
+    <img src="https://chart.googleapis.com/chart?chs=200x200&cht=qr&chld=L|0&chl=bitcoin:<?php echo $ADDR; ?>" alt="" />
+
+    <div id="countdown"></div>
+    <div id="result">Esperando pagamento...</div>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            var url = "https://blockchain.info/";
+            var bal;
+
+            function checkBalance() {
+                $.ajax({
+                    type: "GET",
+                    url: url + "q/getreceivedbyaddress/<?php echo $ADDR; ?>",
+                    data : {format : 'plain'},
+                    success: function(response) {
+                        if (!response) return;
+
+                        var value = parseInt(response);
+
+                        if (value > 0) {
+                            $('#result').text("Recebido!");
+                            $('#countdown').text('');
+                            clearInterval(timer);
+                            clearInterval(bal);
+                        } else {
+                            bal = setTimeout(checkBalance, 5000);
+                        }
+                    }
+                });
+            }
+            ///Check for incoming payment
+            bal = setTimeout(checkBalance, 5000);
+
+
+            var timer;
+            var end = new Date('<?php echo $ENDTIME; ?>');
+
+            function countDown() {
+                var second = 1000;
+                var minute = second * 60;
+                var hour = minute * 60;
+                var day = hour * 24;
+                var now = new Date();
+                var distance = end - now;
+
+                if (distance < 0) {
+                    clearInterval(timer);
+                    clearInterval(bal);
+                    $('#countdown').text('Expirou!');
+                    $('#result').text('');
+
+                    return;
+                }
+
+                var days = Math.floor(distance / day);
+                var hours = Math.floor((distance % day) / hour);
+                var minutes = Math.floor((distance % hour) / minute);
+                var seconds = Math.floor((distance % minute) / second);
+
+                var expire = "Expira em: " + minutes + ':';
+                    expire += (seconds < 10 ? '0' : '') + seconds;
+
+                $('#countdown').text(expire);
+            }
+
+            // inicia funcao countDown
+            countDown();
+            timer = setInterval(countDown, 1000);
+        });
+    </script>
+<?php else : ?>
+    <h1>Doação</h1>
+    <form action="<?php $_SERVER["PHP_SELF"] ?>" method="POST">
+        <ul>
+            <li>
+                <button name="btn" type="submit">Doar</button>
+            </li>
+        </ul>
+    </form>
+<?php endIf ?>
+</body>
+</html>
